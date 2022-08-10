0223533e865de580d83c0049f02d2577dd4d8712
Beko Pharm <arduino-joystick@beko.famkos.net>
2022-08-10 21:48:49 +0200

    Make code more readable

--- joystick-firmware/joystick-firmware.ino
+++ joystick-firmware/joystick-firmware.ino
@@ -3,13 +3,10 @@ const int pin_JoystickLY = A1;
 const int pin_JoystickRX = A2;
 const int pin_JoystickRY = A3;
 
-#define BUTTON_PACKAGE 16
 // D-PAD + ABXY + START & SELECT + L1 & R1 + L2 & R2 + 34 TriggerHappies
-// !! Must be 3 * BUTTON_PACKAGE !!
+// !! Must be 3 * 16 !!
 // Reading the buttons in bunches of 16 later
-#define NUM_BUTTONS (4 + 4 + 2 + 2 + 2 + 34)
-
-int buttons[(NUM_BUTTONS)] = {
+int buttons[48] = {
    2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 22, 23,
   24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
   38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
@@ -33,7 +30,7 @@ void setup() {
   pinMode(pin_JoystickRX, INPUT);
   pinMode(pin_JoystickRY, INPUT);
 
-  for (int i = 0; i < NUM_BUTTONS; i++) {
+  for (int i = 0; i < 48; i++) {
     pinMode(buttons[i], INPUT_PULLUP);
   }
 }
@@ -41,10 +38,10 @@ void setup() {
 void loop() {
   uint16_t payload[8];
 
-  payload[0] = 65535; // header: 16 1s (2 byte)
+  payload[0] = UINT16_MAX; // header: 16 1s (2 byte)
 
   for (uint8_t i = 0; i < 3; i++) { // 6 bytes buttons
-    payload[(i+1)] = readButtonState(i);
+    payload[(i+1)] = readButtonStateBatch(i);
   }
 
   for (uint8_t i = 0; i < 4; i++) { // 8 bytes axes
@@ -63,11 +60,11 @@ void loop() {
 /**
 * Reads the buttons in bunches of 16 starting with a multiply of x
 */
-uint16_t readButtonState(uint8_t x) {
+uint16_t readButtonStateBatch(uint8_t x) {
   uint16_t buttonState = 0;
   uint8_t c = 0;
 
-  for (uint8_t i = (x * BUTTON_PACKAGE); i < (x * BUTTON_PACKAGE + BUTTON_PACKAGE); i++) {
+  for (uint8_t i = (x * 16); i < (x * 16 + 16); i++) {
     if (digitalRead(buttons[i]) == LOW) {
       buttonState |= (1 << c);
     }
